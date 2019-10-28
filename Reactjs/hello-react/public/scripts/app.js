11ec681744f6bccde4fcf485ad50cf2b79a1674e
littleguppies <sang.lequang94@gmail.com>
2019-10-28 14:05:57 +0700

    Add enclosing tag in JSX

--- Reactjs/hello-react/public/scripts/app.js
+++ Reactjs/hello-react/public/scripts/app.js
@@ -1,9 +1,34 @@
 "use strict";
 
+console.log("app.js is running");
+
 var template = React.createElement(
-  "h1",
+  "div",
   null,
-  "Indecision App"
+  React.createElement(
+    "h1",
+    null,
+    "Indecision App"
+  ),
+  React.createElement(
+    "p",
+    null,
+    "Hello this is p tag"
+  ),
+  React.createElement(
+    "ol",
+    null,
+    React.createElement(
+      "li",
+      null,
+      "The first"
+    ),
+    React.createElement(
+      "li",
+      null,
+      "The second"
+    )
+  )
 );
 var appRoot = document.getElementById("app");
 
