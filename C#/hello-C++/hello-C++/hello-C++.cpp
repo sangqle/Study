62f700fc86b0e857bd10160d6a7472de8c2a3eed
littleguppies <sang.lequang94@gmail.com>
2019-11-17 07:56:47 +0700

    LTM add code.pdf file

---
+++ C#/hello-C++/hello-C++/hello-C++.cpp
@@ -0,0 +1,13 @@
+// hello-C++.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout<< "Hello C++";
+    return 0;
+}
