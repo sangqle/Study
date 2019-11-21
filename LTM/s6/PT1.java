0061c956f6a6f5ce386ae1bb4d5ea1f00093e052
littleguppies <sang.lequang94@gmail.com>
2019-11-21 16:54:36 +0700

    LTM-s6 deleted .class files

--- LTM/s6/PT1.java
+++ LTM/s6/PT1.java
@@ -5,32 +5,38 @@ public class PT1 extends UnicastRemoteObject implements PT1_Itf {
 	public PT1() throws RemoteException {
 		super();
 	}
+
 	// cai dat ham goi tu xa
 	public String GiaiPT1(float a, float b) {
-		if(b == 0 && (a == 0 || a != 0)) return "x = 0";
-		else if(a == 0 && b != 0) return "Pt vo nghiem";
-		else return "x = " + Float.toString(-b/a);
+		if (b == 0 && (a == 0 || a != 0))
+			return "x = 0";
+		else if (a == 0 && b != 0)
+			return "Pt vo nghiem";
+		else
+			return "x = " + Float.toString(-b / a);
 	}
+
 	public String KyVong(float[] a) {
 		float sum = 0;
 		float Ea = 0;
 		float Aa = 0;
-		for(int i = 0; i < a.length; i++) {
+		for (int i = 0; i < a.length; i++) {
 			sum += a[i];
 		}
-		Ea = sum/a.length;
-		for(int i = 0; i < a.length; i++) {
+		Ea = sum / a.length;
+		for (int i = 0; i < a.length; i++) {
 			sum += Math.pow((a[1] - Ea), 2);
 		}
 		Aa = sum;
 		return "E = " + Float.toString(Ea) + ", " + "A = " + Float.toString(Aa);
 	}
+
 	public float[][] NhanMatran(float[][] a, float[][] b) {
 		float[][] c = new float[a.length][b[0].length];
-		for(int i = 0; i < a.length; i++) {
-			for(int j = 0; j < a[i].length; j++) {
-				for(int t = 0; t < b[j].length; t++) {
-					c[i][t] += a[i][j] * b[j][t]; 
+		for (int i = 0; i < a.length; i++) {
+			for (int j = 0; j < a[i].length; j++) {
+				for (int t = 0; t < b[j].length; t++) {
+					c[i][t] += a[i][j] * b[j][t];
 				}
 			}
 		}
--- LTM/s6/PT1_Itf.java
+++ LTM/s6/PT1_Itf.java
@@ -1,7 +1,10 @@
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+
 public interface PT1_Itf extends Remote {
 	public String GiaiPT1(float a, float b) throws RemoteException;
+
 	public String KyVong(float[] a) throws RemoteException;
+
 	public float[][] NhanMatran(float[][] a, float[][] b) throws RemoteException;
 }
