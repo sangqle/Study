f4c8ceb4b6366f7c071632e0a12f185babd787ed
littlemonster00 <sang.lequang94@gmail.com>
2019-12-04 04:12:36 +0700

    QuanLyQuanCafe add Date checkout

--- CSharp Winform/QuanLyQuanCafe/QuanLyQuanCafe/DAO/BillDAO.cs
+++ CSharp Winform/QuanLyQuanCafe/QuanLyQuanCafe/DAO/BillDAO.cs
@@ -41,7 +41,7 @@ namespace QuanLyQuanCafe.DAO
 
         public void CheckOut(int id, int discount)
         {
-            string query = "UPDATE dbo.Bill SET status = 1, " + "discount = " + discount + "WHERE id = " + id;
+            string query = "UPDATE dbo.Bill SET dateCheckOut = GETDATE(), status = 1, " + "discount = " + discount + "WHERE id = " + id;
             DataProvider.Instance.ExecuteNonQuery(query);
         }
 
