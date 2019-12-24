3e56044cd296a1481e5aec9b150a3c0733fb8c16
littlemonster00 <sang.lequang94@gmail.com>
2019-12-24 09:46:54 +0700

    Interview cutomize multiple big number

--- Code-interview/mulbignum.js
+++ Code-interview/mulbignum.js
@@ -6,32 +6,31 @@ const mulbignum = (a, b) => {
   const strB = b.toString().split("");
 
   const arrRq = [];
-  const arrRowSum = new Array(strB.length + strA.length).fill(0);
-  const arrRowResult = new Array(strB.length + strA.length).fill(0);
+  const rowSum = new Array(strB.length + strA.length).fill(0);
+  const rowResult = new Array(strB.length + strA.length).fill(0);
 
   for (let i = strB.length - 1; i >= 0; i--) {
-    const arrRow = new Array(strB.length + strA.length).fill(0);
+    const row = new Array(strB.length + strA.length).fill(0);
     for (let j = strA.length - 1; j >= 0; j--) {
       const result = parseInt(strB[i], 10) * parseInt(strA[j], 10);
-      arrRow[i + j + 1] = result;
-      arrRowSum[i + j + 1] += result;
+      row[i + j + 1] = result;
+      rowSum[i + j + 1] += result;
     }
-    arrRq.push(arrRow);
+    arrRq.push(row);
   }
-  arrRq.push(arrRowSum);
+  arrRq.push(rowSum);
 
   // divide the remainder by ten
-  for (let i = arrRowSum.length - 1; i >= 0; i--) {
-    if (arrRowSum[i] > 9) {
-      arrRowSum[i - 1] += Math.floor(arrRowSum[i] / 10);
-      arrRowResult[i] = arrRowSum[i] % 10;
+  for (let i = rowSum.length - 1; i >= 0; i--) {
+    if (rowSum[i] > 9) {
+      rowSum[i - 1] += Math.floor(rowSum[i] / 10);
+      rowResult[i] = rowSum[i] % 10;
     } else {
-      arrRowResult[i] = arrRowSum[i];
+      rowResult[i] = rowSum[i];
     }
   }
   console.log(a * b);
-  return arrRowResult.join("");
+  return rowResult.join("");
 };
 
 console.log(mulbignum(a, b));
-
