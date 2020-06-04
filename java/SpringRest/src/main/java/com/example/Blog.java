347dad9e28074b0ebcfb09912c59508fd5aa8cb6
littlemonster00 <sang.lequang94@gmail.com>
2020-06-04 21:03:31 +0700

    Spring: Connect mysql server

--- java/SpringRest/src/main/java/com/example/Blog.java
+++ java/SpringRest/src/main/java/com/example/Blog.java
@@ -1,6 +1,11 @@
 
 package com.example;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Blog {
     private int id;
     private String title;
